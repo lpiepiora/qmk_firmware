52dfdad0cf0423781e1b584c0d63542420b417c7
Lukasz Piepiora <lpiepiora@gmail.com>
2023-12-27 20:57:35 +0100

    Customize keymap

---
+++ keyboards/sofle/keymaps/lpiepiora/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define OLED_BRIGHTNESS 64
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
