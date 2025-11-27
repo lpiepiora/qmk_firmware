f5692f55af683c763bc67d4f8f63033164b6fc8a
Lukasz Piepiora <lpiepiora@gmail.com>
2025-11-27 12:47:57 +0100

    Configure Anna58

---
+++ keyboards/anna58/readme.md
@@ -0,0 +1,11 @@
+# Anna 58
+
+Anna 58 is 6×4+5keys column-staggered split keyboard with layout matching Lily58.
+
+Build & flash left side:
+
+    $ qmk flash -kb anna58/rev1 -km default -bl uf2-split-left
+
+Build & flash right side:
+
+    $ qmk flash -kb anna58/rev1 -km default -bl uf2-split-right
---
+++ keyboards/anna58/rev1/config.h
@@ -0,0 +1,23 @@
+// Copyright 2025 lpiepiora
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+
+#pragma once
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
+
+#define SERIAL_PIO_USE_PIO0
+#define SERIAL_USART_FULL_DUPLEX
+
+#define EE_HANDS
+
+#ifdef INIT_EE_HANDS_LEFT
+#   define SERIAL_USART_TX_PIN GP15
+#   define SERIAL_USART_RX_PIN GP14
+#endif
+
+#ifdef INIT_EE_HANDS_RIGHT
+#   define SERIAL_USART_TX_PIN GP1
+#   define SERIAL_USART_RX_PIN GP0
+#endif
+
